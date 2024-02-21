a588ef43cf4cbf220c43ddcfe736e742761269a0
[Jitendra Kumar] <[jitendra427@gmail.com]>
2024-02-21 18:32:32 +0530

    Add source code

---
+++ Input-Output.cs
@@ -0,0 +1,37 @@
+﻿/****************************************************************
+*************************** INPUT *******************************
+****************************************************************/
+
+
+//****** Getting a String Input​ ******
+
+Console.WriteLine("Enter your name:");
+
+//Type your name and press enter
+string nameInput = Console.ReadLine();
+
+Console.WriteLine("Hello, " + nameInput + "!"); // Prints the user's name
+
+
+
+//****** Getting Numeric Input​ ******
+
+Console.WriteLine("Enter your age:");
+
+//Type your age and press enter
+string ageInput = Console.ReadLine();
+int age = Convert.ToInt32(ageInput); // Convert the string to an integer
+
+Console.WriteLine("Your age is: " + age);
+
+
+/****************************************************************
+*************************** Output ******************************
+****************************************************************/
+
+//This method prints text followed by a newline character (moves to the next line).
+Console.WriteLine("Hello World!");
+
+//Similar to WriteLine() but without inserting a new line at the end of the output.
+Console.Write("Hello Estudy247! ");
+Console.Write("I would like to print the text on the same line.");
